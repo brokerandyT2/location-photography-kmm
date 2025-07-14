28e88d709e1893c3576f25ed49a02340b493af67
Andrew Winn <brokerandy25@gmail.com>
2025-07-14 09:14:03 -0400

    aaaa

--- photography/build.gradle.kts
+++ photography/build.gradle.kts
@@ -148,7 +148,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
+    @Suppress("UnstableApiUsage")
     testOptions {
         unitTests.isReturnDefaultValues = true
     }
